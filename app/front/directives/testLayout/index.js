e2362359f02d75adc2ac1708e1093eb4295db159
Rody <Rody10@mail.ru>
2016-02-21 23:11:46 +0600

    Made a new design used angular material. (test-layout)

--- app/front/directives/testLayout/index.js
+++ app/front/directives/testLayout/index.js
@@ -1,27 +1,48 @@
 (function () {
     angular.module('app.directives.testLayout', [])
-        .directive('testLayout', function () {
+        .directive('testLayout', function ($mdSidenav) {
             return {
                 restrict: 'E',
                 templateUrl: 'directives/testLayout/index.html',
                 scope: true,
+                bindToController: true,
+                controllerAs: "vm",
                 controller: function ($scope, $window) {
                     var navig = angular.element($window);
                     $scope.align = "center center";
 
-                    navig.bind('resize', function(e){
+                    navig.bind('resize', function (e) {
                         var newWidth = navig.width();
                         $scope.winWidth = newWidth;
-                        if (newWidth <= 980){
+                        if (newWidth <= 1280) {
                             $scope.align = "start center";
                             $scope.$apply();
+                            $mdSidenav('left').close()
                         } else {
                             $scope.align = "center center";
                             $scope.$apply();
                         }
 
                     });
+
+                    var previous = null;
+                    $scope.selectTool = function (e) {
+                        if (previous) {
+                            previous.toggleClass('test-selected-tool');
+                        }
+                        angular.element(e.target.children[1]).toggleClass('test-selected-tool');
+                        previous = angular.element(e.target.children[1]);
+                        $scope.toggleLeft();
+                    };
+
+                    $scope.toggleLeft = function () {
+                        $scope.toggleSlidePanel('left');
+                    };
+
+                    $scope.toggleSlidePanel = function (panelid) {
+                        $mdSidenav(panelid).toggle();
+                    };
                 }
             }
-        });
+        })
 })();
---
+++ app/front/directives/toolBar/mainFrame.js
@@ -0,0 +1,35 @@
+(function () {
+    angular.module('app.directives.mainframe', [])
+        .directive('mainFrame', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                templateUrl: 'directives/mainFrame/mainFrame.html',
+
+                link: function (scope, element) {
+
+                },
+                controller: function ($scope, $rootScope, $document) {
+                    $scope.selectedTool = "";
+
+                    $scope.selectInToolbar = function(toolName){
+                        $('.toolbar').toggleClass("open-toolbar");
+                        $scope.selectedTool = toolName;
+                        $rootScope.$broadcast('selected-additional-tool');
+                    };
+
+                    $scope.$on('selected-main-tool', function (event, data) {
+                        $scope.selectedTool = '';
+                    });
+
+                    $document.on("click", function(event){
+                        if ($(event.target).attr('id') != "label-short-name" ||
+                            $(!event.target).parent().hasClass('tool')){
+                            $('.toolbar').removeClass("open-toolbar");
+                        }
+
+                    });
+                }
+            }
+        });
+})();
